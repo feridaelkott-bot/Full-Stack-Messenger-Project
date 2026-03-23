ca88fd216739e47822f43136e94ac17449c62f76
mashencia <mashencia.malic@gmail.com>
2026-03-23 10:26:21 -0400

    add: DatabaseManager, config setup and pom dependencies

---
+++ backend_code/my-javalin-project/src/main/java/com/mygroup/DatabaseManager.java
@@ -0,0 +1,64 @@
+package com.mygroup;
+
+//imports
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseManager {
+
+    //The single data pool (shared by all threads): originally null until init() called
+    private static HikariDataSource dataSource;
+
+    //Creates the pool fails under critical conditions
+    public static void init() {
+        Properties properties = new Properties();
+
+        //Reading of the config.properties
+        try(InputStream input = DatabaseManager.class.getClassLoader().getResourceAsStream("config.properties")){
+            if (input == null) throw new RuntimeException("config.properties not found.");
+            properties.load(input);
+        }catch(Exception e){
+            throw new RuntimeException("Configurations failed to load: "+ e.getMessage());
+        }
+
+        HikariConfig config = new HikariConfig();
+
+        //JDBC: url tells the driver to use postgresql on the localhost:5432 server connecting to messenger_db
+        config.setJdbcUrl("jdbc:postgresql://"
+                + properties.getProperty("db.host") + ":"
+                + properties.getProperty("db.port") + "/"
+                + properties.getProperty("db.name"));
+        config.setUsername(properties.getProperty("db.user"));
+        config.setPassword(properties.getProperty("db.password"));
+
+        //maximum number of connections that can be open on PostgreSQL
+        config.setMaximumPoolSize(10);
+
+        //connections that are open even if there are no users
+        config.setMinimumIdle(2);
+
+        //30 seconds of wait time before error if all connections are occupied
+        config.setConnectionTimeout(30_000);
+
+        //10 minutes of unused connection it will be closed
+        config.setIdleTimeout(600_000);
+
+        dataSource = new HikariDataSource(config);
+        System.out.println("Connected to database (PostgreSQL: localhost:5432)");
+    }
+
+    //Every class calls to borrow a connection from the pool
+    public static Connection getConnection() throws SQLException {
+        if (dataSource == null) throw new IllegalStateException("database not initialized");
+        return dataSource.getConnection();
+    }
+
+    //Closes the connections
+    public static void close() {
+        if (dataSource != null) dataSource.close();
+    }
+}
