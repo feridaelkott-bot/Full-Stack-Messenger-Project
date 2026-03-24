c0c977b0505355a20db94f22f97a215bba3265f5
Ujwal Bastola <ubastola90@gmail.com>
2026-03-24 12:02:39 -0400

    Fixed login issues, edited a bit of backend to make sure the frontend and backend are communicating properly, added ! to PostgresUserRepo to make it accept registered info, updated gitIgnore

--- backend_code/my-javalin-project/src/main/java/com/mygroup/MessengerApp.java
+++ backend_code/my-javalin-project/src/main/java/com/mygroup/MessengerApp.java
@@ -1,67 +1,67 @@
 package com.mygroup; //the archetype created this package sturcture, based on my group id
 
 //now let's import javalin, and since we have its dependencies inside the pom.xml file, it should work 
-import io.javalin.Javalin; 
-import java.util.Map; 
+import io.javalin.Javalin;
+import java.util.Map;
 import io.javalin.websocket.WsContext;
 import java.util.concurrent.ConcurrentHashMap;
 import com.google.gson.Gson; //import the Gson library for JSON parsing --> possible only when we added the Maven dependency
-import java.util.ArrayList; 
+import java.util.ArrayList;
 
 //these will be for parsing the JSON string for only the type field first
-import com.google.gson.JsonParser; 
+import com.google.gson.JsonParser;
 import com.google.gson.JsonObject;
 
-public class MessengerApp 
+public class MessengerApp
 {
 
-        //track all of the users in a concurrent hash map 
-        //-->This will be useful for handling all of the connections from a single program. 
+        //track all of the users in a concurrent hash map
+        //-->This will be useful for handling all of the connections from a single program.
         //-->ConcurrentHashMap is useful since many clients can connect at once --> concurrent hash maps are thread-safe and allow multiple threads to access them at once*/
-        
+
 
         private static final Map<WsContext, String> ctx_and_phones = new ConcurrentHashMap<>();
         private static final Map<WsContext, String> userUsernameMap = new ConcurrentHashMap<>();
         private static final ArrayList<String> allNumbers = new ArrayList<>(); //this array will never have its numbers taken out, unless the user deletes their account
 
 
-        //each client gets their own copy of each of the classes below: 
+        //each client gets their own copy of each of the classes below:
 
-        //From the main page: 
+        //From the main page:
         public class LoginInfo{
 
             String type; //login
-            String phone_number; 
-            String password; 
+            String phone_number;
+            String password;
 
         }
 
 
         public class RegisterInfo{
             String type; //register
-            String phone_number; 
-            String password; 
+            String phone_number;
+            String password;
         }
 
         //the new message button is found in teh main page screen
         public class Message{
             String type; //new message
-            int toPhone; 
+            int toPhone;
             int fromPhone; //should be automatically filled in by the backend
         }
 
         //these are the three button implementations for each chat that the user has:
         public class Remove{
             String type; //remove
-            String phone_number; 
-        } 
+            String phone_number;
+        }
         public class Block{
             String type; //block
-            String phone_number; 
+            String phone_number;
         }
         public class Report{
             String type;//report
-            String phone_number; 
+            String phone_number;
         }
 
 
@@ -72,42 +72,44 @@ public class MessengerApp
 
         public class DeleteAccount{
             String type; //deleteAcc
-            String phone_number; 
-        
-        }//if this is the type, then delete the information from the database, then remove the ctx connection. 
+            String phone_number;
+
+        }//if this is the type, then delete the information from the database, then remove the ctx connection.
 
 
 
     public static void main( String[] args )
     {
-
+        // Initialize Database and Repository
+        DatabaseManager.init();
+        PostgresUserRepo userRepo = new PostgresUserRepo();
         //create the websocket, which will be the server that all clients connect to:
         Javalin app = Javalin.create()//create the websocket server, and its
 
-            
+
             //define the websocket endpoint --> you only see this if you enter the url into the browser
-            .get("/", ctx -> ctx.result("Hello World! This server is working :)")) //Creates a web browser for our server. CTX here is the HTTP request context NOT a client context. NOT required in our program, but useful for testing. 
-            
+            .get("/", ctx -> ctx.result("Hello World! This server is working :)")) //Creates a web browser for our server. CTX here is the HTTP request context NOT a client context. NOT required in our program, but useful for testing.
+
 
             //websocket connection object
-            .ws("/", ws -> { 
+            .ws("/", ws -> {
 
                 //this on connect part is triggered by teh line buildAsync(URI...("ws://localhost:7070/")) from the GUI
                 ws.onConnect(ctx -> {
-                    
+
 
                     ctx_and_phones.put(ctx, ""); //empty phone for now
 
                     //*At this point, the  user is on the main page of the GUI, where they have the option to login/register*/
                     //*when GUI sends any json, then it can ONLY be read inside onMessage
-                 
 
-                    
-                    
-                    
+
+
+
+
                     //Future Reference: ctx has several methods to work with the specific user connection
                 });
-                
+
                 ws.onMessage(ctx -> {
 
                     //*The actual JSON strings would be gotten from the line ctx.message() */
@@ -115,8 +117,8 @@ public class MessengerApp
 
                     String rawJson = ctx.message(); //get the json that is sent from the GUI
                     //in JavaFX, there is a certain line: .buildAsync(URI.create("ws://localhost:7070/"), new WebSocket.Listener() {
-                    //this line above is part of the method in teh GUI file that is for connecting to the server. 
-                    //this is the point where GUI JSON input can be sent to the backend server here. 
+                    //this line above is part of the method in teh GUI file that is for connecting to the server.
+                    //this is the point where GUI JSON input can be sent to the backend server here.
                     //so ctx.message() gets triggered by the line "webSocket.sendText(jsonOutput, true);" from teh GUI
 
 
@@ -126,68 +128,62 @@ public class MessengerApp
 
                     //get the 'type' of the json params
                     Gson gson = new Gson();
-                    if (type.equals("login")){ //if the JSON is a login request
-                        
-                        LoginInfo new_user = gson.fromJson(rawJson, LoginInfo.class); //convert the JSON string to a LoginInfo object, which has the fields: type, username, password, phone number
-                        
+                    if (type.equals("login")){
+                        LoginInfo new_user = gson.fromJson(rawJson, LoginInfo.class);
 
-                        //find out if the phone number is inside the phone numbers ArrayList
-                        if (allNumbers.contains(new_user.phone_number)){
-                            //todo: database checks if "new_user.password" exists
-                            boolean exists_in_db = true; 
-                            if (exists_in_db){
-                                //add this newly connected number to the array of ctx and phone number objects: 
-                                ctx_and_phones.put(ctx, new_user.phone_number); //if the ctx already exists, then this would overwrite it
-                            }
+                        // DATABASE CHECK
+                        AuthResult result = userRepo.login(new_user.phone_number, new_user.password);
 
-                        }else{
-                            //if the entered phone number is not inside the allNumbers array list, then send error message to GUI
-                            //todo: JavaFX takes this information from the onText method
-                            ctx.send("The entered phone number does not exist. Click to register instead: "); 
-
+                        if (result.success){
+                            ctx_and_phones.put(ctx, new_user.phone_number);
+                            ctx.send("{\"type\": \"login_success\", \"phone\": \"" + new_user.phone_number + "\"}");
+                        } else {
+                            ctx.send("{\"type\": \"login_error\", \"message\": \"" + result.message + "\"}");
                         }
-
                     }
                     else if(type.equals("message")){
                         Message new_message = gson.fromJson(rawJson, Message.class); //convert the JSON string to a Message object, which has the fields: type, username, text
-
-
                     }
                     else if (type.equals("register")){
-                        RegisterInfo new_register = gson.fromJson(rawJson, RegisterInfo.class); //convert the JSON string to a RegisterInfo object, which has the fields: type, username, password, phone number
-                        allNumbers.add(new_register.phone_number); //append the new number to the array list of phone numbers
+                        RegisterInfo new_register = gson.fromJson(rawJson, RegisterInfo.class);
 
-                        //todo: database needs to add teh phone, and password
+                        // DATABASE REGISTRATION (Using phone_number for the username field too)
+                        AuthResult result = userRepo.register(new_register.phone_number, new_register.password, new_register.phone_number);
 
+                        if (result.success) {
+                            ctx.send("{\"type\": \"register_success\", \"message\": \"" + result.message + "\"}");
+                        } else {
+                            ctx.send("{\"type\": \"register_error\", \"message\": \"" + result.message + "\"}");
+                        }
                     }else if (type.equals("block")){
-                        
-                        Block blocked = gson.fromJson(rawJson, Block.class); 
-                        String connection_to_remove = blocked.phone_number; 
+
+                        Block blocked = gson.fromJson(rawJson, Block.class);
+                        String connection_to_remove = blocked.phone_number;
 
                         //todo: database removes the connection between the blocked and the user
 
                     }else if (type.equals("report")){
-                        Report reported = gson.fromJson(rawJson, Report.class); 
+                        Report reported = gson.fromJson(rawJson, Report.class);
                         //todo: ?????
                     }
                     else if (type.equals("logout")){
-                        Logout logging_out = gson.fromJson(rawJson, Logout.class); 
+                        Logout logging_out = gson.fromJson(rawJson, Logout.class);
                         //get the number that is logging out, remove its ctx connection, and remove its number from the array list of numbers
                         userUsernameMap.remove(ctx); //remove their conection fromt eh map of connections
                         ctx.send("You are being disconnected"); //todo: GUI needs to accept this message
                         ctx.session.close(); //todo: make sure teh GUI can detect this close and close its own window accordingly
-                    
+
                     }else if(type.equals("deleteAcc")){
                         DeleteAccount deleteAcc = gson.fromJson(rawJson, DeleteAccount.class);
-                        userUsernameMap.remove(ctx); 
-                        ctx.send("Deleting account..."); 
-                        ctx.session.close(); 
+                        userUsernameMap.remove(ctx);
+                        ctx.send("Deleting account...");
+                        ctx.session.close();
 
-                        //todo: database deletes the phone number, and their password, and all fo their messages. 
+                        //todo: database deletes the phone number, and their password, and all fo their messages.
                     }
-                    
+
                 });
-                
+
                 ws.onError(ctx -> {
                     System.out.println("An error occurred: "); //print the error message if an error occurs
                 });
@@ -195,13 +191,13 @@ public class MessengerApp
                 ws.onClose(ctx -> {
                     System.out.println(ctx.sessionId() + " disconnected"); //print the session id of the client that just disconnected
                     userUsernameMap.remove(ctx); //remove the client from the map when they disconnect
-                }); 
+                });
 
             })
-            .start(7070); 
+            .start(7070);
 
-        //define the websocket connection's endpoint: 
-        
-       
+        //define the websocket connection's endpoint:
+
+
     }
 }
